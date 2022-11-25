833c28811cfd6e5528111c4622259ecd253aaa59
unknown <anamika362890@gmail.com>
2022-11-25 14:06:22 +0600

    category

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dqus0zu.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        const categoryCollection = client.db('bookWagon').collection('categories');
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const options = await categoryCollection.find(query).toArray();
+            res.send(options);
+        })
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
