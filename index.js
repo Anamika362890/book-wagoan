b1e4d570097818de3ba32bec3630b7c9be3af188
unknown <anamika362890@gmail.com>
2022-11-26 18:02:25 +0600

    addProducts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -19,12 +19,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const categoryCollection = client.db('bookWagon').collection('categories');
+        const productsCollection = client.db('bookWagon').collection('products');
+        const usersCollection = client.db('bookWagon').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const options = await productsCollection.find(query).toArray();
+            res.send(options);
+        })
+
+
+
+
 
         app.get('/category', async (req, res) => {
             const query = {};
             const options = await categoryCollection.find(query).toArray();
             res.send(options);
         })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await categoryCollection.findOne(query);
+            res.send(service);
+
+        })
+
 
     }
 
